cd023ea1ae918bbf81214d93cd1e960034a46325
Jomol MJ <mj.jomol6@gmail.com>
2022-03-25 21:37:35 +0530

    changed the api url into a variable

--- src/api/book.ts
+++ src/api/book.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+const API_URL = 'https://the-dune-api.herokuapp.com/';
+
 export const getBooks = () => {
-    return axios.get(`https://the-dune-api.herokuapp.com/books/21`);
+    return axios.get(`${API_URL}books/21`);
 }
 
 export const getBook = (bookId: string) => {
-    return axios.get(`https://the-dune-api.herokuapp.com/books/id/${bookId}`);
+    return axios.get(`${API_URL}books/id/${bookId}`);
 }
